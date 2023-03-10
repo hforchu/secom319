c29b4fef9090f91685f1cbba55f6e1498b51b3d6
hforchu <harveyforchu@gmail.com>
2023-03-10 17:26:05 -0600

    final work for midterm

--- product_catalog/jackets.js
+++ product_catalog/jackets.js
@@ -1,4 +1,4 @@
-fetch('./data.json')
+fetch('./jackets.json')
     .then(function (response) {
         return response.json();
     })
@@ -34,7 +34,7 @@ function appendData(data) {
                       <button
                         type="button"
                         class="btn btn-sm btn-outline-secondary"
-                        onclick="window.open('${result.img_url}', 'targetWindow', 'toolbar=no, location=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=1090px, height=550px, top=25px left=120px'); return false;"
+                        onclick=""
                       >
                         View
                       </button>
